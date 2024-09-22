3e65685a322f2d4683310f0669392d17a21885cc
randyc92 <costarandy92@gmail.com>
2024-09-22 16:51:17 -0400

    correction

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
 import random
 
-suits = ["Spades", "Heerts", "Diamonds", "Clubs"]
+suits = ["Spades", "Hearts", "Diamonds", "Clubs"]
 
 cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
 
