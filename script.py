59603ff82e62b1cf63de1907b3cb71c52e78b56c
randyc92 <costarandy92@gmail.com>
2024-09-22 15:05:40 -0400

    create deck

--- script.py
+++ script.py
@@ -2,4 +2,8 @@ import random
 
 suits = ["Spades", "Heerts", "Diamonds", "Clubs"]
 
-cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "King"]
+cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
+
+deck = [(suit, card) for suit in suits for card in cards]
+
+print(deck)
