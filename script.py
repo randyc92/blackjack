39c952dec0408ad965065740e82eff10273ea2bb
randyc92 <costarandy92@gmail.com>
2024-09-22 15:09:43 -0400

    card values function

--- script.py
+++ script.py
@@ -6,4 +6,12 @@ cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
 
 deck = [(suit, card) for suit in suits for card in cards]
 
-print(deck)
+def card_values(card):
+    if card[0] == ["Jack", "Queen", "King"]:
+        value = 10
+    elif card[0] == "Ace":
+        value = 11
+    else:
+        value = int(card[0])
+    
+    return value
